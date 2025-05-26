d2d69f4de268a8df3dafc708ae283c11b0fb7b08
dongho <dongho7736@gmail.com>
2025-05-26 15:46:53 +0900

    오랜만

---
+++ ex18-01a.js
@@ -0,0 +1,5 @@
+const arr = [11, 22, 33, 44, 55]
+const arr2 = arr
+console.log(arr, arr2)
+console.log(arr == arr2)
+console.log(arr === arr2)
---
+++ ex18-01d.js
@@ -0,0 +1,12 @@
+const user = {name:"홍길동", age:10}
+
+console.log(user.name)
+console.log(user['name'])
+
+user.gender = "male"
+user['address'] = "부산"
+
+user.age = 17
+user['name'] = "슈퍼맨"
+
+delete['name']
